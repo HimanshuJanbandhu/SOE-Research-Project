7a065a5d905ee2f9c1f40198f6f128d2d94bb230
HimanshuJanbandhu <hv.13janbandhu@gmail.com>
2020-03-01 16:37:25 +0530

    N Gram

--- src/net/codejava/jdbc/JdbcSQLiteConnection.java
+++ src/net/codejava/jdbc/JdbcSQLiteConnection.java
@@ -25,7 +25,14 @@ class Graph {
 
     public Map<Vertex,Boolean> visited = new Hashtable<Vertex,Boolean>();
 
+    public Map<Vertex, String> Summary = new Hashtable<Vertex, String>();
+
+    public Map<Vertex, String> Description = new Hashtable<Vertex, String>();
+
+    Vertex for_ngram;
+
     String AICIA[] = {"issue_id","Component","issue_id","assignee"};
+    String DICIF[] ={"issue_id","Component","issue_id"};
 
     Vertex currentSource;
 
@@ -57,17 +64,17 @@ class Graph {
         }
     }
     public int aicia=0;
-    public void DFS_MetaPaths(Vertex source,int step){
+    public void DFS_MetaPaths(Vertex source,int step, String path[]){
         //System.out.print(source.Name + " ");
         visited.replace(source,true);
-        if ((source.type.compareTo(AICIA[AICIA.length-1]) == 0)  && !(currentSource.Name.compareTo(source.Name)==0)) {
+        if ((source.type.compareTo(path[path.length-1]) == 0)  && !(currentSource.Name.compareTo(source.Name)==0)) {
             //System.out.println(source.Name);
             aicia++;
         }
         for(Vertex v: adjVertices.get(source)){
             if(step<AICIA.length) {
-                 if ((v.type.compareTo(AICIA[step]) == 0) && !visited.get(v)) {
-                    DFS_MetaPaths(v, 1+step);
+                 if ((v.type.compareTo(path[step]) == 0) && !visited.get(v)) {
+                    DFS_MetaPaths(v, 1+step,path);
                 }
             }
         }
@@ -75,16 +82,16 @@ class Graph {
     }
     int pathCount=0;
     Vertex A,B;
-    public void CountPaths(Vertex u,Vertex v,int step){
+    public void CountPaths(Vertex u,Vertex v,int step, String path[]){
         //visited.replace(u,true);
         if(u==v){
             pathCount++;
         }
         else{
             for(Vertex V: adjVertices.get(u)){
-                if(step<AICIA.length){
-                    if (/*!visited.get(V) &&*/ (V.type.compareTo(AICIA[step])==0 )) {
-                        CountPaths(V,v,1+step);
+                if(step<path.length){
+                    if (/*!visited.get(V) &&*/ (V.type.compareTo(path[step])==0 )) {
+                        CountPaths(V,v,1+step,path);
                     }
                 }
             }
@@ -93,13 +100,103 @@ class Graph {
     }
 }
 
+class Ngrams {
+
+    public static <String> Set<String> mergeSet(Set<String> a, Set<String> b)
+    {
+
+        // Creating an empty set
+        Set<String> mergedSet = new HashSet<String>();
+
+        // add the two sets to be merged
+        // into the new set
+        mergedSet.addAll(a);
+        mergedSet.addAll(b);
+
+        // returning the merged set
+        return mergedSet;
+    }
+
+    public Vertex A,B;
+
+    public Map<String, Integer> Ngrams(int n, String str) {
+        Map<String, Integer> ngrams = new Hashtable<String, Integer>();
+        for (int i = 0; i < str.length() - n + 1; i++) {
+            String s = str.substring(i, i + n);
+            if(ngrams.containsKey(s)){
+                ngrams.put(s,ngrams.get(s)+1);
+            }
+            else{
+                ngrams.put(s,1);
+            }
+        }
+        return ngrams;
+    }
+
+    public void Print_Ngrams(Map<String,Integer> ngrams1, Map<String,Integer> ngrams2){
+        Set<String> Set_Ngrams = mergeSet(ngrams1.keySet(),ngrams2.keySet());
+        int no_of_Ngrams1;
+        int no_of_Ngrams2;
+        for(String s : Set_Ngrams){
+            if(ngrams1.containsKey(s)){
+                no_of_Ngrams1=ngrams1.get(s);
+            }
+            else{
+                no_of_Ngrams1=0;
+            }
+            if(ngrams2.containsKey(s)){
+                no_of_Ngrams2=ngrams2.get(s);
+            }
+            else{
+                no_of_Ngrams2=0;
+            }
+            System.out.printf("%s   %d   %d\n",s,no_of_Ngrams1,no_of_Ngrams2);
+        }
+    }
+}
 public class JdbcSQLiteConnection {
 
     public static void main(String[] args) {
         try {
+                Class.forName("org.sqlite.JDBC");
+                //======archiva=====//
+                String dbURL = "jdbc:sqlite:DatabasesSOE/archiva.sqlite3";
+                Connection conn = DriverManager.getConnection(dbURL);
+                Statement stm = conn.createStatement();
+                Graph graph= new Graph();
+                //=====issue====//
+                ResultSet rs = stm.executeQuery("SELECT * FROM issue");
+                while(rs.next()){
+                    String issue_id,assignee_username;
+                    issue_id = rs.getString("issue_id");
+                    assignee_username = rs.getString("assignee_username");
+                    if(assignee_username!=null) {
+                        graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(assignee_username, "assignee"));
+                    }
+                }
+                //=====issue====//
+                //=====issue_component=====//
+                rs = stm.executeQuery("SELECT * FROM issue_component");
+                while(rs.next()){
+                    String issue_id,component;
+                    issue_id = rs.getString("issue_id");
+                    component = rs.getString("component");
+                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "Component"));
+                }
+                //=====issue_component=====//
+                //=====issue_comment=====//
+                rs = stm.executeQuery("SELECT * FROM issue_comment");
+                while(rs.next()){
+                    String issue_id,comment;
+                    issue_id = rs.getString("issue_id");
+                    comment = rs.getString("username");
+                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(comment, "comment"));
+                }
+                //=====issue_comment=====//
+                //graph.printGraph();
+                //count aicia
 //            Class.forName("org.sqlite.JDBC");
-//            //======archiva=====//
-//            String dbURL = "jdbc:sqlite:DatabasesSOE/archiva.sqlite3";
+//            String dbURL = "jdbc:sqlite:DatabasesSOE/testdb";
 //            Connection conn = DriverManager.getConnection(dbURL);
 //            Statement stm = conn.createStatement();
 //            Graph graph= new Graph();
@@ -113,8 +210,6 @@ public class JdbcSQLiteConnection {
 //                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(assignee_username, "assignee"));
 //                }
 //            }
-//            //=====issue====//
-//            //=====issue_component=====//
 //            rs = stm.executeQuery("SELECT * FROM issue_component");
 //            while(rs.next()){
 //                String issue_id,component;
@@ -122,68 +217,61 @@ public class JdbcSQLiteConnection {
 //                component = rs.getString("component");
 //                graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "Component"));
 //            }
-//            //=====issue_component=====//
-//            //=====issue_comment=====//
-//            rs = stm.executeQuery("SELECT * FROM issue_comment");
-//            while(rs.next()){
-//                String issue_id,comment;
-//                issue_id = rs.getString("issue_id");
-//                comment = rs.getString("username");
-//                graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(comment, "comment"));
-//            }
-//            //=====issue_comment=====//
-//            //graph.printGraph();
-//            //count aicia
-//            for(Vertex v : graph.adjVertices.keySet()){
-//                if((!graph.visited.get(v)) && (v.type.compareTo("assignee")==0)){
-//                    graph.DFS_AICIA(v,0);
-//                }
-//            }
-//            System.out.println(graph.aicia);
-            Class.forName("org.sqlite.JDBC");
-            String dbURL = "jdbc:sqlite:DatabasesSOE/testdb";
-            Connection conn = DriverManager.getConnection(dbURL);
-            Statement stm = conn.createStatement();
-            Graph graph= new Graph();
-            //=====issue====//
-            ResultSet rs = stm.executeQuery("SELECT * FROM issue");
-            while(rs.next()){
-                String issue_id,assignee_username;
-                issue_id = rs.getString("issue_id");
-                assignee_username = rs.getString("assignee_username");
-                if(assignee_username!=null) {
-                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(assignee_username, "assignee"));
-                }
-            }
-            rs = stm.executeQuery("SELECT * FROM issue_component");
-            while(rs.next()){
-                String issue_id,component;
-                issue_id = rs.getString("issue_id");
-                component = rs.getString("component");
-                graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "Component"));
-            }
             //graph.printGraph();
             for(Vertex v : graph.adjVertices.keySet()){
                 if( (v.type.compareTo("assignee")==0)){
                     graph.currentSource = v;
-                    //System.out.print(v.Name);
-                    graph.DFS_MetaPaths(v,0);
+                    graph.DFS_MetaPaths(v,0,graph.AICIA);
                 }
-                //System.out.println("");
-                //System.out.println((graph.aicia));
             }
             System.out.println((graph.aicia/2));
             for(Vertex v : graph.adjVertices.keySet()){
-                if(v.Name.compareTo("C")==0){
+                if(v.Name.compareTo("joakime")==0 && v.type.compareTo("assignee")==0){
                     graph.A=v;
                 }
-                else if(v.Name.compareTo("D")==0){
+                else if(v.Name.compareTo("brettporter")==0 && v.type.compareTo("assignee")==0){
                     graph.B=v;
                 }
             }
-            graph.CountPaths(graph.A,graph.B,0);
+            graph.CountPaths(graph.A,graph.B,0,graph.AICIA);
             System.out.println(graph.pathCount);
+            //======N grams=====//
+            rs = stm.executeQuery("SELECT * FROM issue");
+            while(rs.next()){
+                String summary,description,issue_id;
+                issue_id = rs.getString("issue_id");
+                summary = rs.getString("summary");
+                description = rs.getString("description");
+                for(Vertex v : graph.adjVertices.keySet()){
+                    if(v.Name.compareTo(issue_id)==0 && v.type.compareTo("issue_id")==0){
+                        graph.for_ngram = v;
+                        break;
+                    }
+                }
+                if(summary==null){
+                    summary=" ";
+                }
+                if(description==null){
+                    description=" ";
+                }
+                graph.Summary.put(graph.for_ngram,summary);
+                graph.Description.put(graph.for_ngram,description);
+            }
+            Ngrams Ngrams = new Ngrams();
+            for(Vertex v : graph.adjVertices.keySet()){
+                if(v.Name.compareTo("MRM-415")==0 && v.type.compareTo("issue_id")==0){
+                    graph.A=v;
+                }
+                else if(v.Name.compareTo("MRM-485")==0 && v.type.compareTo("issue_id")==0){
+                    graph.B=v;
+                }
+            }
 
+            String s1 = graph.Summary.get(graph.A) + graph.Description.get(graph.A);
+            String s2 = graph.Summary.get(graph.B) + graph.Description.get(graph.B);
+
+            Ngrams.Print_Ngrams(Ngrams.Ngrams(2,s1),Ngrams.Ngrams(2,s2));
+            //=====N grams=====//
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (SQLException ex) {
