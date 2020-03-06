20425a2827da9254ebd56c005b16b9c3ade76b5b
HimanshuJanbandhu <hv.13janbandhu@gmail.com>
2020-03-06 08:23:19 +0530

    Metapaths and features

--- src/net/codejava/jdbc/JdbcSQLiteConnection.java
+++ src/net/codejava/jdbc/JdbcSQLiteConnection.java
@@ -80,10 +80,17 @@ class Graph {
         }
         visited.replace(source,false);
     }
+
+    public int getAicia(Vertex source,String path[]) {
+        aicia=0;
+        DFS_MetaPaths(source,1,path);
+        return aicia;
+    }
+
     int pathCount=0;
     Vertex A,B;
     public void CountPaths(Vertex u,Vertex v,int step, String path[]){
-        //visited.replace(u,true);
+        visited.replace(u,true);
         if(u==v){
             pathCount++;
         }
@@ -96,8 +103,65 @@ class Graph {
                 }
             }
         }
-        //visited.replace(u,false);
+        visited.replace(u,false);
     }
+
+    public int getPathCount(Vertex u,Vertex v,String path[]){
+        pathCount=0;
+        CountPaths(u,v,1,path);
+        return pathCount;
+    }
+
+    public int NormalisedCountPaths(Vertex u,Vertex v, String path[]) {
+        int PCR = getPathCount(u,v,path);
+        String[] revPath = new String[path.length];
+        int j = path.length;
+        for (int i = 0; i < path.length; i++) {
+            revPath[j - 1] = path[i];
+            j = j - 1;
+        }
+        int PCinR = getPathCount(v,u,revPath);
+        int PCRa_ = getAicia(u,path);
+        int PCR_a = getAicia(v,revPath);
+
+        return (PCR + PCinR)/(PCRa_ + PCR_a);
+    }
+
+    public int RandomWalk(Vertex u, Vertex v, String path[]) {
+
+        int PCR = getPathCount(u,v,path);
+        int PCRa_ = getAicia(u,path);
+
+        return  (PCR/PCRa_);
+    }
+
+    public int SymmetricRandomWalk(Vertex u, Vertex v, String path[]) {
+        String[] revPath = new String[path.length];
+        int j = path.length;
+        for (int i = 0; i < path.length; i++) {
+            revPath[j - 1] = path[i];
+            j = j - 1;
+        }
+        return RandomWalk(u,v,path) + RandomWalk(v,u,revPath);
+    }
+}
+
+class MetaPaths {
+    String AICF[] = {"assignee","issue_id","commit_hash","file"};
+    String CICF[] = {"commenter","issue_id","commit_hash","file"};
+    String CICICF[] = {"commenter","issue_id","commenter","issue_id","commit_hash","file"};
+    String AICICF[] = {"assignee","issue_id","commenter","issue_id","commit_hash","file"};
+    String CIAICF[] = {"commenter","issue_id","assignee","issue_id","commit_hash","file"};
+    String AICFCICICF[] = {"assignee","issue_id","commit_hash","file","commit_hash","issue_id","component","issue_id","commit_hash","file"};
+    String _AICICF[] = {"assignee","issue_id","component","issue_id","commit_hash","file"};
+    String _CICICF[] = {"commenter","issue_id","component","issue_id","commit_hash","file"};
+    String AIRICF[] = {"assignee","issue_id","reporter","issue_id","commit_hash","file"};
+    String CIRICF[] = {"commenter","issue_id","reporter","issue_id","commit_hash","file"};
+    String __AICICF[] = {"assignee","issue_id","component","issue_id","commit_hash","file"};
+    String __CICICF[] = {"commenter","issue_id","component","issue_id","commit_hash","file"};
+    String AIAICF[] = {"assignee","issue_id","assignee","assignee","issue_id","commit_hash","file"};
+    String _CIAICF[] = {"commenter","issue_id","assignee","assignee","issue_id","commit_hash","file"};
+    String AICFCICF[] = {"assignee","issue_id","commit_hash","file","commit_hash","issue_id","commit_hash","file"};
 }
 
 class Ngrams {
@@ -167,11 +231,15 @@ public class JdbcSQLiteConnection {
                 //=====issue====//
                 ResultSet rs = stm.executeQuery("SELECT * FROM issue");
                 while(rs.next()){
-                    String issue_id,assignee_username;
+                    String issue_id,assignee_username,reporter_username;
                     issue_id = rs.getString("issue_id");
                     assignee_username = rs.getString("assignee_username");
+                    reporter_username = rs.getString("reporter_username");
                     if(assignee_username!=null) {
                         graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(assignee_username, "assignee"));
+                    }
+                    if(reporter_username!=null) {
+                        graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(reporter_username, "reporter"));
                     }
                 }
                 //=====issue====//
@@ -181,7 +249,7 @@ public class JdbcSQLiteConnection {
                     String issue_id,component;
                     issue_id = rs.getString("issue_id");
                     component = rs.getString("component");
-                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "Component"));
+                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "component"));
                 }
                 //=====issue_component=====//
                 //=====issue_comment=====//
@@ -190,88 +258,66 @@ public class JdbcSQLiteConnection {
                     String issue_id,comment;
                     issue_id = rs.getString("issue_id");
                     comment = rs.getString("username");
-                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(comment, "comment"));
+                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(comment, "commenter"));
                 }
                 //=====issue_comment=====//
-                //graph.printGraph();
-                //count aicia
-//            Class.forName("org.sqlite.JDBC");
-//            String dbURL = "jdbc:sqlite:DatabasesSOE/testdb";
-//            Connection conn = DriverManager.getConnection(dbURL);
-//            Statement stm = conn.createStatement();
-//            Graph graph= new Graph();
-//            //=====issue====//
-//            ResultSet rs = stm.executeQuery("SELECT * FROM issue");
-//            while(rs.next()){
-//                String issue_id,assignee_username;
-//                issue_id = rs.getString("issue_id");
-//                assignee_username = rs.getString("assignee_username");
-//                if(assignee_username!=null) {
-//                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(assignee_username, "assignee"));
+                //=====change_set_link=====//
+                rs = stm.executeQuery("SELECT * FROM change_set_link");
+                while(rs.next()){
+                    String issue_id,commit_hash;
+                    issue_id = rs.getString("issue_id");
+                    commit_hash = rs.getString("commit_hash");
+                    graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(commit_hash, "commit_hash"));
+                }
+                //=====change_set_link=====//
+                //=====code_change=====//
+                rs = stm.executeQuery("SELECT * FROM code_change");
+                while(rs.next()){
+                    String file_path,commit_hash;
+                    file_path = rs.getString("file_path");
+                    commit_hash = rs.getString("commit_hash");
+                    graph.addEdge(graph.addVertex(file_path, "file"),graph.addVertex(commit_hash, "commit_hash"));
+                }
+                //=====code_change=====//
+                //PathCount;
+                //NormalisedPathCount;
+                //======N grams=====//
+//                rs = stm.executeQuery("SELECT * FROM issue");
+//                while(rs.next()){
+//                    String summary,description,issue_id;
+//                    issue_id = rs.getString("issue_id");
+//                    summary = rs.getString("summary");
+//                    description = rs.getString("description");
+//                    for(Vertex v : graph.adjVertices.keySet()){
+//                        if(v.Name.compareTo(issue_id)==0 && v.type.compareTo("issue_id")==0){
+//                            graph.for_ngram = v;
+//                            break;
+//                        }
+//                    }
+//                    if(summary==null){
+//                        summary=" ";
+//                    }
+//                    if(description==null){
+//                        description=" ";
+//                    }
+//                    graph.Summary.put(graph.for_ngram,summary);
+//                    graph.Description.put(graph.for_ngram,description);
 //                }
-//            }
-//            rs = stm.executeQuery("SELECT * FROM issue_component");
-//            while(rs.next()){
-//                String issue_id,component;
-//                issue_id = rs.getString("issue_id");
-//                component = rs.getString("component");
-//                graph.addEdge(graph.addVertex(issue_id, "issue_id"),graph.addVertex(component, "Component"));
-//            }
-            //graph.printGraph();
-            for(Vertex v : graph.adjVertices.keySet()){
-                if( (v.type.compareTo("assignee")==0)){
-                    graph.currentSource = v;
-                    graph.DFS_MetaPaths(v,0,graph.AICIA);
-                }
-            }
-            System.out.println((graph.aicia/2));
-            for(Vertex v : graph.adjVertices.keySet()){
-                if(v.Name.compareTo("joakime")==0 && v.type.compareTo("assignee")==0){
-                    graph.A=v;
-                }
-                else if(v.Name.compareTo("brettporter")==0 && v.type.compareTo("assignee")==0){
-                    graph.B=v;
-                }
-            }
-            graph.CountPaths(graph.A,graph.B,0,graph.AICIA);
-            System.out.println(graph.pathCount);
-            //======N grams=====//
-            rs = stm.executeQuery("SELECT * FROM issue");
-            while(rs.next()){
-                String summary,description,issue_id;
-                issue_id = rs.getString("issue_id");
-                summary = rs.getString("summary");
-                description = rs.getString("description");
-                for(Vertex v : graph.adjVertices.keySet()){
-                    if(v.Name.compareTo(issue_id)==0 && v.type.compareTo("issue_id")==0){
-                        graph.for_ngram = v;
-                        break;
-                    }
-                }
-                if(summary==null){
-                    summary=" ";
-                }
-                if(description==null){
-                    description=" ";
-                }
-                graph.Summary.put(graph.for_ngram,summary);
-                graph.Description.put(graph.for_ngram,description);
-            }
-            Ngrams Ngrams = new Ngrams();
-            for(Vertex v : graph.adjVertices.keySet()){
-                if(v.Name.compareTo("MRM-415")==0 && v.type.compareTo("issue_id")==0){
-                    graph.A=v;
-                }
-                else if(v.Name.compareTo("MRM-485")==0 && v.type.compareTo("issue_id")==0){
-                    graph.B=v;
-                }
-            }
-
-            String s1 = graph.Summary.get(graph.A) + graph.Description.get(graph.A);
-            String s2 = graph.Summary.get(graph.B) + graph.Description.get(graph.B);
-
-            Ngrams.Print_Ngrams(Ngrams.Ngrams(2,s1),Ngrams.Ngrams(2,s2));
-            //=====N grams=====//
+//                Ngrams Ngrams = new Ngrams();
+//                for(Vertex v : graph.adjVertices.keySet()){
+//                    if(v.Name.compareTo("MRM-415")==0 && v.type.compareTo("issue_id")==0){
+//                        graph.A=v;
+//                    }
+//                    else if(v.Name.compareTo("MRM-485")==0 && v.type.compareTo("issue_id")==0){
+//                        graph.B=v;
+//                    }
+//                }
+//
+//                String s1 = graph.Summary.get(graph.A) + graph.Description.get(graph.A);
+//                String s2 = graph.Summary.get(graph.B) + graph.Description.get(graph.B);
+//                System.out.printf("ngrams   %s   %s\n",graph.A.Name,graph.B.Name);
+//                Ngrams.Print_Ngrams(Ngrams.Ngrams(5,s1),Ngrams.Ngrams(5,s2));
+                //=====N grams=====//
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (SQLException ex) {
